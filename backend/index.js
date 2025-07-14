e2dc419aa39f6a207278104e4b8fc75060370ac1
Mukesh Kumar <mukesh512004@gmail.com>
2025-07-14 21:34:57 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,12 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+  ],
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   res.send('Welcome to Mutli-Vendor Project Backend Side!');
