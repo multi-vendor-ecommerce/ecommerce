664cca4d0c3a54c11b2e0e43d40527ea7aebf48b
Palchhin <palchhinparihar@gmail.com>
2025-07-23 15:40:54 +0530

    fix: indent the auth context and state; fix the auth controller name.

--- frontend/src/context/auth/AuthState.jsx
+++ frontend/src/context/auth/AuthState.jsx
@@ -2,80 +2,80 @@ import { useEffect, useState } from "react";
 import AuthContext from "./AuthContext";
 
 const AuthState = ({ children }) => {
-    const [authToken, setAuthToken] = useState(localStorage.getItem("authToken") || null);
-    const [user, setUser] = useState(null);
-    const [authLoading, setAuthLoading] = useState(false);
+  const [authToken, setAuthToken] = useState(localStorage.getItem("authToken") || null);
+  const [user, setUser] = useState(null);
+  const [authLoading, setAuthLoading] = useState(false);
 
-    const host = import.meta.env.VITE_BACKEND_URL;
+  const host = import.meta.env.VITE_BACKEND_URL;
 
-    // fetch user details if token is present
-    useEffect(() => {
-        if (authToken) {
-            getUserDetails();
-        }
-    }, [authToken]);
-
-    const login = async (email, password) => {
-        try {
-            const res = await fetch(`${host}/api/auth/login`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ email, password }),
-            });
-
-            const data = await res.json();
-
-            if (data.success) {
-                localStorage.setItem("authToken", data.authToken);
-                setAuthToken(data.authToken);
-                return { success: true };
-            } else {
-                return { success: false, error: data.error || "Login failed" };
-            }
-
-        } catch (err) {
-            console.error("Login error: ", err);
-            return { success: false, error: "Something went wrong" }
-        }
-    };
-
-    const logout = () => {
-        localStorage.removeItem("authToken");
-        setAuthToken(null);
-        setUser(null);
+  // fetch user details if token is present
+  useEffect(() => {
+    if (authToken) {
+      getUserDetails();
     }
+  }, [authToken]);
 
-    const getUserDetails = async () => {
-        setAuthLoading(true);
-        try {
-            const res = await fetch(`${host}/api/auth/getuser`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "applicaiton/json",
-                    "auth-token": authToken,
-                },
-            });
-            const data = await res.json();
+  const login = async (email, password) => {
+    try {
+      const res = await fetch(`${host}/api/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
 
-            if (data.success) {
-                setUser(data.user);
-            } else {
-                logout();
-            }
-        } catch (err) {
-            console.error("Fetching user error: ", err);
-            logout();
-        } finally {
-            setAuthLoading(false);
-        }
-    };
+      const data = await res.json();
 
-    return (
-        <AuthContext.Provider value={{ authToken, user, authLoading, login, logout }}>
-            {children}
-        </AuthContext.Provider>
+      if (data.success) {
+        localStorage.setItem("authToken", data.authToken);
+        setAuthToken(data.authToken);
+        return { success: true };
+      } else {
+        return { success: false, error: data.error || "Login failed" };
+      }
 
-    )
+    } catch (err) {
+      console.error("Login error: ", err);
+      return { success: false, error: "Something went wrong" }
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("authToken");
+    setAuthToken(null);
+    setUser(null);
+  }
+
+  const getUserDetails = async () => {
+    setAuthLoading(true);
+    try {
+      const res = await fetch(`${host}/api/auth/getuser`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "applicaiton/json",
+          "auth-token": authToken,
+        },
+      });
+      const data = await res.json();
+
+      if (data.success) {
+        setUser(data.user);
+      } else {
+        logout();
+      }
+    } catch (err) {
+      console.error("Fetching user error: ", err);
+      logout();
+    } finally {
+      setAuthLoading(false);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ authToken, user, authLoading, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+
+  )
 }
 
 export default AuthState;
