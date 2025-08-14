291dd11bb13fa0fbb2dccbe12cf88d522490325b
Palchhin <palchhinparihar@gmail.com>
2025-08-14 13:49:09 +0530

    fix: import files in vendor.

--- backend/routes/vendorRoutes.js
+++ backend/routes/vendorRoutes.js
@@ -4,6 +4,7 @@ import authorizeRoles from "../middleware/authorizeRole.js";
 
 // Controllers
 import { getAllVendors, getTopVendors, editStore, getVendorById } from "../controllers/vendorController.js";
+import upload from "../middleware/multer.js";
 import { validate } from "../middleware/validateFields.js";
 
 const router = express.Router();
